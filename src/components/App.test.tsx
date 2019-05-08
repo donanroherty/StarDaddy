f935b8a4658969f012f3b949bb1540c8d7b96ca3
Ronan Doherty <donanroherty@gmail.com>
2019-05-08 14:31:54 +0100

    Added libs for styling and testing

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { render, cleanup } from 'react-testing-library'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+afterEach(() => cleanup)
+
+it('<App /> renders', () => {
+  const component = render(<App />)
+  expect(component).toBeTruthy()
+})
