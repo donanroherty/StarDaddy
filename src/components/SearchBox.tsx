fe15079b3c093445a9bdd28ee59e84b0fbffcdfa
Ronan Doherty <donanroherty@gmail.com>
2019-05-23 10:51:45 +0100

    Styled search box

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,20 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
 import { useSearch } from '../state/search-context'
+import { GoSearch } from 'react-icons/go'
 
 const SearchBox = () => {
   const { searchTerm, setSearchTerm } = useSearch()
 
   return (
-    <div data-testid="search-box">
-      <input
-        type="text"
-        placeholder="Search..."
-        id="search"
-        onChange={event => setSearchTerm(event.target.value)}
-        value={searchTerm}
-      />
-    </div>
+    <Wrapper data-testid="search-box">
+      <InputField>
+        <input
+          type="text"
+          placeholder="Search..."
+          id="search"
+          onChange={event => setSearchTerm(event.target.value)}
+          value={searchTerm}
+        />
+
+        <GoSearch size="24px" />
+      </InputField>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  border: 0.5px solid ${({ theme }) => theme.color.border};
+  border-radius: 15px;
+  margin: 12.8px;
+  box-sizing: border-box;
+`
+const InputField = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 12px 12px 12px 20px;
+  > input {
+    font-size: 18px;
+    border: none;
+    background-color: transparent;
+    color: ${({ theme }) => theme.color.text};
+  }
+`
+const SearchIconWrapper = styled.div``
 
 export default SearchBox
