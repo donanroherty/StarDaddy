ef7b80d5a6a9090eec214766ef6153ec91f282fc
Ronan Doherty <donanroherty@gmail.com>
2019-08-13 11:26:52 +0100

    Added confirmation popup to 'Clear local data' option

--- src/components/SettingsMenu.tsx
+++ src/components/SettingsMenu.tsx
@@ -4,6 +4,7 @@ import useAppState from 'state/hooks/useAppState'
 import useGithub from 'state/hooks/useGithub'
 import useSettings from 'state/hooks/useSettings'
 import useOutsideClick from 'hooks/useOutsideClick'
+import usePopup from 'state/hooks/usePopup'
 
 interface SettingsMenuProps {
   settingsBtnRef: React.RefObject<HTMLDivElement>
@@ -13,6 +14,7 @@ const SettingsMenu: React.FC<SettingsMenuProps> = ({ settingsBtnRef }) => {
   const { user } = useAppState()
   const { logout } = useGithub()
   const { settingsMenuOpen, hideSettingsMenu, openAboutModal } = useSettings()
+  const { showConfirmPopup, handleCancel } = usePopup()
 
   const ref = useRef<HTMLDivElement>(null)
 
@@ -39,8 +41,21 @@ const SettingsMenu: React.FC<SettingsMenuProps> = ({ settingsBtnRef }) => {
         >
           About
         </SettingItem>
-            logout(true)
-            hideSettingsMenu()
+
+        <SettingItem
+          onClick={() => {
+            showConfirmPopup(
+              <div>Clear all locally saved data and settings?</div>,
+              true,
+              [0, 0],
+              () => {
+                logout(true)
+                hideSettingsMenu()
+              },
+              () => {
+                handleCancel()
+              }
+            )
           }}
         >
           Clear local storage
